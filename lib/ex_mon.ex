0473934863c55ef8dc53fbad9cd99bdb7002dc8c
arturbernardo <artur.bernardo.k@gmail.com>
2024-03-10 13:20:52 -0300

    Agents

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,9 +1,20 @@
 defmodule ExMon do
   # podemos criar um alias e chamar, como quisermos, outros modulos
   # alias ExMon.Player, as: Banana
+  alias ExMon.{Game, Player}
+  alias ExMon.Game.Status
+
+  @computer_name "Robotinik"
 
   def create_player(name, move_rnd, move_avg, move_heal) do
-    # Banana.build
-    ExMon.Player.build(name, move_rnd, move_avg, move_heal)
+    Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player(:punch, :kick, :heat)
+    |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
